9e7e13a18ebbe9ab83bedee57e9b15bd4f1772a7
dzm <1837043171@qq.com>
2020-12-17 18:22:18 +0800

    项目目录结构设置

---
+++ src/pages/order.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+import NavHeader from '../components/NavHeader.vue'
+export default {
+    name:'home'
+}
+</script
+        NavHeader>
+
+<style lang="stylus">
+
+</style>
